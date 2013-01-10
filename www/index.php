0ad22848befd776d1e89d2efd3e0e651acbc9ea2
Krunal Patel <k5patel@me.com>
2013-01-10 07:23:46 +0000

    pdated index

--- www/index.php
+++ www/index.php
@@ -5,22 +5,25 @@ set_time_limit(0);
 
 if (isset($_POST['doit'])) {
   //defaults
-  $build = "build02";
+  $build = $_POST['build'];
+  $g = $_POST['grid'];
   $width = $_POST['width'];
   $height = $_POST['height'];
-
-  $n = 1;
-  $z = 1;
-  $p = 1;
+  $n = $_POST['samples'];
+  $z = $_POST['zoom'];
   $o = "/rgrt/www/images/" . uniqid('rgrt-', true) . "-rgrt.bmp";
 
   //ok execute the rgrt-cuda program.  
-  echo shell_exec("export LD_LIBRARY_PATH=/usr/local/cuda-5.0/lib:/usr/local/cuda-5.0/lib64:\$LD_LIBRARY_PATH;cd /rgrt;bin/linux/release/rgrt-cuda m=1 -g=1 -b=" . $build . " -w=" . $width . " -h=" . $height . " -n=" . $n . " -z=" . $z . " -p=" . $p . " -o=" . $o);
+  shell_exec("export LD_LIBRARY_PATH=/usr/local/cuda-5.0/lib:/usr/local/cuda-5.0/lib64:\$LD_LIBRARY_PATH;cd /rgrt;bin/linux/release/rgrt-cuda m=1 -g=". $g ." -b=" . $build . " -w=" . $width . " -h=" . $height . " -n=" . $n . " -z=" . $z . " -p=0 -o=" . $o);
 
   //now use the Linux at command to delete the newly created file after 15 minutes
   shell_exec("export SHELL=/bin/bash && echo rm -f " . $o . " | at now + 15 minutes");
 
+  //sleep for a sec
+  sleep(2);
 
+  //now show the user the image:
+  header('Location: images' . strrchr($o,'/'));
 
   exit;
 
@@ -40,32 +43,35 @@ if (isset($_POST['doit'])) {
   <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
   <link rel="stylesheet" href="style.css" />
   <script>
+
   $(function() {
       $( "#slider-width" ).slider({
 	range: "max",
 	    min: 16,
-	    max: 8192,
+	    max: 4096,
 	    value: 512,
-	    step: 4,
+	    step: 2,
 	    slide: function( event, ui ) {
 	    $( "#width" ).val( ui.value );
 	  }
 	});
       $( "#width" ).val( $( "#slider-width" ).slider( "value" ) );
     });
+
   $(function() {
       $( "#slider-height" ).slider({
 	range: "max",
 	    min: 16,
-	    max: 8192,
+	    max: 4096,
 	    value: 512,
-	    step: 4,
+	    step: 2,
 	    slide: function( event, ui ) {
 	    $( "#height" ).val( ui.value );
 	  }
 	});
       $( "#height" ).val( $( "#slider-height" ).slider( "value" ) );
     });
+
   $(function() {
       $( "#slider-samples" ).slider({
 	range: "max",
@@ -80,6 +86,20 @@ if (isset($_POST['doit'])) {
       $( "#samples" ).val( $( "#slider-samples" ).slider( "value" ) );
     });
 
+  $(function() {
+      $( "#slider-zoom" ).slider({
+	range: "max",
+	    min: -1024,
+	    max: 1024,
+	    value: 1,
+	    step: 1,
+	    slide: function( event, ui ) {
+	    $( "#zoom" ).val( ui.value );
+	  }
+	});
+      $( "#zoom" ).val( $( "#slider-zoom" ).slider( "value" ) );
+    });
+
 
   </script>
 </head>
@@ -87,6 +107,29 @@ if (isset($_POST['doit'])) {
  
 <form action="index.php" method="post">
 <input type="hidden" name="doit" value="doit">
+
+<p>
+<label>Build File:</label>
+<select name="build">
+<option value="build01">1</option>
+<option value="build02">2</option>
+<option value="build03">3</option>
+<option value="build04">4</option>
+<option value="build05">5</option>
+<option value="build06">6</option>
+<option value="build07">7</option>
+<option value="build08">8</option>
+<option value="build09">9</option>
+</select>
+</p>
+
+<p>
+<label>Grid Acceleration:</label>
+<select name="grid">
+<option value="1">Yes</option>
+<option value="0">No</option>
+</select>
+</p>
 
 <p>
   <label>Width:</label>
@@ -106,12 +149,13 @@ if (isset($_POST['doit'])) {
 </p>
 <div id="slider-samples"></div>
 
-
-
-
+<p>
+  <label>Zoom:</label>
+  <input type="text" id="zoom" name="zoom" style="border: 0; color: #f6931f; font-weight: bold;" />
+</p>
+<div id="slider-zoom"></div>
 
 <br/>
-
 <input type="submit" value="Submit">
 
 </form>
